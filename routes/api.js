acf4f3e4c56dc00a9342b1e5c51c7cc933a8287e
greenpoop <apple88501@gmail.com>
2025-10-05 21:56:41 -0700

    add angular front end and index page

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/hello', (req, res) => {
-  res.json({ message: 'Hello from /api/hello!' });
+  res.json({ message: 'Hello from API!' });
 });
 
 module.exports = router;
