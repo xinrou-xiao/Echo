5eecae8c47c3ffc00c345ad9d072f88058572d45
Shanvisinha <shanvisinha98@gmail.com>
2025-11-13 17:27:53 -0800

    Added navigation to view_profile is _id exists

--- client/src/app/components/header/header.ts
+++ client/src/app/components/header/header.ts
@@ -16,8 +16,17 @@ export class Header {
     private readonly router: Router
   ) {}
 
-  protected goToProfile(): void {
-    void this.router.navigate(['/profile']);
+   protected goToProfile(): void {
+    const me = this.auth.profile();
+
+    // if no user or no _id yet send them to the profile form
+    if (!me || !me._id) {
+      void this.router.navigate(['/profile']);
+      return;
+    }
+
+    // otherwise send them to view_profile/:id to display profile data
+    void this.router.navigate(['/view_profile', me._id]);
   }
 
   protected async login(): Promise<void> {
